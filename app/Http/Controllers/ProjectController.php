4c4284ab1f0659ff3754e9e950d7b3911a31b8e1
YanAsolole <alfianyt282@gmail.com>
2023-08-29 14:14:06 +0700

    tambah api

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProjectApi;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class ProjectApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('project.index',[
+            'project' => ProjectApi::getDataFromAPI()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('project.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data_project = $request->validate([
+            'nama_project' => ['required', 'numeric'],
+            'tugas' => ['required'],
+            'deskripsi' => ['required'],
+            'tgl_mulai' => ['required'],
+            'tgl_selesai' => ['required']
+        ]);
+    
+        // Panggil metode postDataToAPI() dengan data yang divalidasi
+        ProjectApi::postDataToAPI($data_project);
+    
+        return redirect('index')->with('success', 'Data project Berhasil di Tambah');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id, ProjectApi $project)
+    {
+        return view('project.edit',[
+            'project' => $project
+        ]); 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $data_project = $request->validate([
+            'nama_project' => ['required', 'numeric'],
+            'tugas' => ['required'],
+            'deskripsi' => ['required'],
+            'tgl_mulai' => ['required'],
+            'tgl_selesai' => ['required']
+        ]);
+    
+        // Panggil metode postDataToAPI() dengan data yang divalidasi
+        ProjectApi::updateDataInAPI($id, $data_project);
+    
+        return redirect('index')->with('success', 'Data project Berhasil di Update');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProjectApi $project)
+    {
+        
+    }
+}
---
+++ app/Models/ProjectApi.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GuzzleHttp\Client;
+
+class ProjectApi extends Model
+{
+    use HasFactory;
+    const API_ENDPOINT = 'https://klikyuk.com/ngankngonk/fadli/project-pkl/api/project.php/';
+
+    public static function getDataFromAPI()
+    {
+        $client = new Client();
+        $response = $client->get(self::API_ENDPOINT);
+        $data = json_decode($response->getBody(), true);
+
+        return $data;
+    }
+
+    public static function postDataToAPI($postData)
+    {
+        $client = new Client();
+        $response = $client->post(self::API_ENDPOINT, [
+            'json' => $postData
+        ]);
+
+        return $response->getStatusCode(); // Returns the HTTP status code
+    }
+
+    public static function updateDataInAPI($id, $updatedData)
+    {
+        $client = new Client();
+        $response = $client->put(self::API_ENDPOINT . '/' . $id, [
+            'json' => $updatedData
+        ]);
+
+        return $response->getStatusCode(); // Returns the HTTP status code
+    }
+
+    public static function deleteDataInAPI($id)
+    {
+        $client = new Client();
+        $response = $client->delete(self::API_ENDPOINT . '/' . $id);
+
+        return $response->getStatusCode(); // Returns the HTTP status code
+    }
+}
