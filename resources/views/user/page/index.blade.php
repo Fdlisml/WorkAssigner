eb70bd5198b62e6e7960b4a2e1a7c177533f9faa
zenzyyyy <yusril.nanditama14@gmail.com>
2023-09-18 10:39:03 +0700

    we

--- resources/views/user/page/index.blade.php
+++ resources/views/user/page/index.blade.php
@@ -6,7 +6,7 @@
         <div class="recentOrders">
             <div class="cardHeader">
                 <h2>Jobs Today</h2>
-                <a href="laporan" class="btn">Working Result</a>
+                <a id="myBtn" class="btn">Form Laporan</a>
             </div>
 
             <div class="wadah-table">
@@ -34,62 +34,67 @@
                             <td>{{ $tugas['tgl_selesai'] }}</td>
                         </tr>
                     </tbody>
-                    
                 </table>
             </div>
         </div>
 
         <!-- ================= New Customers ================ -->
-        <div class="recentCustomers">
-            <div class="cardHeader">
-                <h2>Job Report</h2>
-            </div>
+        <div id="myModal" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+                <div class="recentCustomers">
+                    <div class="cardHeader">
+                        <h2>Job Report</h2>
+                        <span class="close">&times;</span>
+                    </div>
+                    <div class="wadah-form">
+                        <div class="container-form">
+                            <p>
+                                <ion-icon name="business-outline"></ion-icon><span>Job Management</span>
+                            </p>
+                            <hr>
+                            <form action="{{ url('user/laporan/store') }}" method="POST">
+                                @csrf
+                                <div class="center-form">
+                                    <label>Report Name</label><br>
+                                    <input type="text" name="nama_laporan" placeholder="Masukan Nama Laporan" required>
+                                    <br>
+                                    <br>
+                                    <label>Report Description</label><br>
+                                    <textarea name="deskripsi" cols="30" rows="5" placeholder="Masukan Laporan" required></textarea>
+                                    <br>
+                                    <br>
+                                    <label>Complaint</label><br>
+                                    <textarea name="keluhan" cols="30" rows="5" placeholder="Masukan Keluhan" required></textarea>
+                                    <br>
+                                    <br>
+                                    <label>Progress</label><br>
+                                    <div class="field">
+                                        <div class="range-active">
+                                            <input class="range" type="range" name="progres" min="0"
+                                                max="100" value="0" steps="1">
+                                        </div>
 
-            <div class="wadah-form">
-                <div class="container-form">
-                    <p>
-                        <ion-icon name="business-outline"></ion-icon><span>Job Management</span>
-                    </p>
-                    <hr>
-                    <form action="{{ url('user/laporan/store') }}" method="POST">
-                        @csrf
-                        <div class="center-form">
-                            <label>Report Name</label><br>
-                            <input type="text" name="nama_laporan" placeholder="Masukan Nama Laporan" required>
-                            <br>
-                            <br>
-                            <label>Report Description</label><br>
-                            <textarea name="deskripsi" cols="30" rows="5" placeholder="Masukan Laporan" required></textarea>
-                            <br>
-                            <br>
-                            <label>Complaint</label><br>
-                            <textarea name="keluhan" cols="30" rows="5" placeholder="Masukan Keluhan" required></textarea>
-                            <br>
-                            <br>
-                            <label>Progress</label><br>
-                            <div class="field">
-                                <div class="range-active">
-                                    <input class="range" type="range" name="progres" min="0" max="100"
-                                        value="0" steps="1">
+                                        <div class="value">
+                                            <span class="rangeValue">0%</span>
+                                        </div>
+                                    </div>
                                 </div>
-
-                                <div class="value">
-                                    <span class="rangeValue">0%</span>
+                                <div class="btn-form">
+                                    <input type="hidden" name="tgl_laporan"
+                                        value="{{ \Carbon\Carbon::now()->format('Y-m-d') }}">
+                                    <input type="hidden" name="id_tugas" value="{{ $tugas['id'] }}">
+                                    <input type="hidden" name="id_user" value="{{ session('id') }}">
+                                    <button class="learn-more" type="submit">
+                                        <span class="circle" aria-hidden="true">
+                                            <span class="icon arrow"></span>
+                                        </span>
+                                        <span class="button-text">Send!</span>
+                                    </button>
                                 </div>
-                            </div>
+                            </form>
                         </div>
-                        <div class="btn-form">
-                            <input type="hidden" name="tgl_laporan" value="{{ \Carbon\Carbon::now()->format('Y-m-d') }}">
-                            <input type="hidden" name="id_tugas" value="{{ $tugas['id'] }}">
-                            <input type="hidden" name="id_user" value="{{ session('id') }}">
-                            <button class="learn-more" type="submit">
-                                <span class="circle" aria-hidden="true">
-                                    <span class="icon arrow"></span>
-                                </span>
-                                <span class="button-text">Send!</span>
-                            </button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
